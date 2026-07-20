67875ff66a1178c007b741191df25d33377be6f2
Ivan Batrakov <blackfan321@gmail.com>
2026-07-20 21:17:27 +0300

    update to 3.68.44

--- express.nix
+++ express.nix
@@ -7,11 +7,11 @@
 
 let
   pname = "express";
-  version = "3.68.41";
+  version = "3.68.44";
 
   src = fetchurl {
     url = "https://updates.express.ms/desktop/eXpress-${version}.AppImage";
-    hash = "sha256-hZmlbyXob2oT6XjefWLK7N8+nnteOqD5QVge1PwPF8Y=";
+    hash = "sha256-z/aG5M7/3RezH7/XSJF8yor79ChzYb+MqXOAO71mFnc=";
   };
 
   appimageContents = appimageTools.extractType1 { inherit pname version src; };
